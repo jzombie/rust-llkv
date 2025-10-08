fb8ccb6cb878becc3b69b0fb19e57b8f12b91bff
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-10-08 15:11:58 -0500

    Add TODO

--- llkv-sql/src/lib.rs
+++ llkv-sql/src/lib.rs
@@ -1,6 +1,8 @@
 pub type SqlResult<T> = llkv_result::Result<T>;
 
 mod sql_engine;
+
+// TODO: Rename to `sql_value`
 mod value;
 
 pub use llkv_dsl::{SelectExecution, StatementResult, TransactionKind};
